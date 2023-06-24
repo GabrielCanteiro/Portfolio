08a7185d5dd580756f7219bc95d3f4c4904e800a
GabrielCanteiro <gabrielc896@gmail.com>
2023-06-24 17:52:59 +0100

    experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,18 +4,26 @@ export const Experience = () => {
       <h1 className="font-bebas_neue text-3xl">Experience</h1>
 
       <div className="mt-5 flex flex-wrap justify-between ">
-        <div>
+        <div className="mb-5">
           <h2 className="font-now_bold uppercase">RW Interactive</h2>
           <p>- Jr. React Developer, 2023 - Present</p>
         </div>
-        <div>
-          <h2 className="font-now_bold uppercase mt-5 sm:mt-0">Villares Metals SA</h2>
+        <div className="mb-5">
+          <h2 className="font-now_bold uppercase  ">Freelancer</h2>
+          <p>- Front End Developer, 2020 - 2023</p>
+        </div>
+        <div className="mb-5">
+          <h2 className="font-now_bold uppercase ">
+            Villares Metals SA
+          </h2>
           <p>- Chemical Analyst II, 2022 - 2023</p>
           <p>- Chemical Analyst I, 2019 - 2022</p>
           <p>- Trainee, 2018 - 2019</p>
         </div>
         <div></div>
         <div></div>
+        <div></div>
+        
       </div>
     </div>
   );
